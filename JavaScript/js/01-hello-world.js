4f301dacec014e0f200f3f44711ac16a1831b890
Edanur Kocakoc <125741593+edanurkkoc@users.noreply.github.com>
2024-07-15 17:18:18 +0300

    15.07.2024
    Patika front-end eğitimlerine devam edildi,css ödevleri tamamlandı.

---
+++ JavaScript/js/01-hello-world.js
@@ -0,0 +1,7 @@
+//***********Merhaba Dunya Ornekleri*************/
+//Dokumana (document) Yazdırma İslemi ile Merhaba Dunya:
+// document.write('Selammm')
+// Uyarı Mesajı (Alert) ile Merhaba Dunya:
+// alert("Merhaba Dunya");
+//console.log ile Merhaba Dunya:
+console.log("Merhaba Dünya")
